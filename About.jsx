91cd4d83f0acbecc1c08034e52bc5ad84a6f80db
Muhammad Khan <123129086+MRehanK@users.noreply.github.com>
2024-12-20 18:33:08 -0500

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -1,19 +1,1 @@
-import React from "react";
 
-function About() {
-  return (
-    <div className="container mx-auto mt-20 p-4">
-      <h1 className="text-3xl font-bold mb-6">About Us</h1>
-      <p>
-        Welcome to our Bookstore platform! We are passionate about providing
-        quality resources to help individuals grow their skills and knowledge.
-      </p>
-      <p className="mt-4">
-        Our mission is to empower learners with free and affordable courses and
-        books in various fields. Stay curious and keep learning!
-      </p>
-    </div>
-  );
-}
-
-export default About;
